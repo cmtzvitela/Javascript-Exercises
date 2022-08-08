65651532eac1f262f6469f9e31e5e06fad13928d
unknown <cmtzvitela@gmail.com>
2022-08-08 13:41:28 -0500

    Completed the first question from Chapter 5

--- Chapter05/Question01.js
+++ Chapter05/Question01.js
@@ -1,6 +1,10 @@
 class Client {
-  constructor(name, accountID, ownMoney, balance) {
-    (this.name = name), (this.accountID = accountID), (this.ownMoney = ownMoney), (this.balance = balance);
+  constructor(name, alias, accountID, ownMoney, balance) {
+    (this.name = name),
+      (this.alias = alias),
+      (this.accountID = accountID),
+      (this.ownMoney = ownMoney),
+      (this.balance = balance);
   }
   accountInfo(ID) {
     if (ID == this.Client.accountID) {
@@ -27,12 +31,25 @@ class Client {
       return;
     }
   }
-  depositAnother(money, ID) {
-    if (money <= this.ownMoney) {
-      let client = accountInfo(ID);
-      console.log(client);
-      if (client.accountID in bank.client) {
-        return client.accountID.balance + money;
+  depositAnotherfromOwnMoney(money, sender, receiver) {
+    if (money <= sender.ownMoney) {
+      if (receiver.alias in bank) {
+        sender.ownMoney = sender.ownMoney - money;
+        console.log(`$${money} were transfered to ${receiver.name}`);
+        console.log(`Now you are carrying ${sender.ownMoney}`);
+        return (receiver.balance = receiver.balance + money);
+      }
+    } else {
+      console.log("Insufficient funds. Try another amount");
+    }
+  }
+  depositAnotherfromBalance(money, sender, receiver) {
+    if (money <= sender.balance) {
+      if (receiver.alias in bank) {
+        sender.balance = sender.balance - money;
+        console.log(`$${money} were transfered to ${receiver.name}`);
+        console.log(`Now you are carrying ${sender.balance}`);
+        return (receiver.balance = receiver.balance + money);
       }
     } else {
       console.log("Insufficient funds. Try another amount");
@@ -42,19 +59,19 @@ class Client {
 
 let x = 2000;
 let y = 2500;
-const client1 = new Client("James", 12345, 3000, 1800);
-const client2 = new Client("Sarah", 98765, 5000, 2500);
+const client1 = new Client("James", "client1", 12345, 3000, 1800);
+const client2 = new Client("Sarah", "client2", 98765, 5000, 2500);
+const client3 = new Client("Amelia", "client3", 78563, 1000, 6500);
 console.log(client1.depositOwn(x));
 console.log(client1.withdraw(y));
 
 const bank = {
   client1: client1,
   client2: client2,
-  98765: {
-    balance: client2.balance,
-  },
+  client3: client3,
 };
 
+console.log(bank.client3);
 console.log(bank);
-console.log(client1.depositAnother(500, "98765"));
-console.log(client2.balance);
+console.log(client1.depositAnotherfromOwnMoney(850, client1, client2));
+console.log(client1.depositAnotherfromBalance(850, client1, client2));
