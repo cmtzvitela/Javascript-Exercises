e6fa9cd31e4e0aeac2759f8835cfdeaf7ef68679
unknown <cmtzvitela@gmail.com>
2022-08-09 16:55:40 -0500

    Second question of Chapter 5 ready

--- Chapter05/Question02.js
+++ Chapter05/Question02.js
@@ -1,162 +1,86 @@
-var roads = [
-  "Alice's House-Bob's House",
-  "Alice's House-Cabin",
-  "Alice's House-Post Office",
-  "Bob's House-Town Hall",
-  "Daria's House-Ernie's House",
-  "Daria's House-Town Hall",
-  "Ernie's House-Grete's House",
-  "Grete's House-Farm",
-  "Grete's House-Shop",
-  "Marketplace-Farm",
-  "Marketplace-Post Office",
-  "Marketplace-Shop",
-  "Marketplace-Town Hall",
-  "Shop-Town Hall",
+const building = [
+  [
+    //First floor
+    [
+      //Office 101
+      { type: "person", name: "Alice" },
+      { type: "equipment", name: "Alice's Computer", belongsTo: "Alice" },
+    ],
+    [
+      //Office 102
+      { type: "person", name: "Fred" },
+      { type: "equipment", name: "Fred's Computer", belongsTo: "Fred" },
+    ],
+  ],
+  [
+    //Second floor
+    [
+      //Office 201
+      { type: "person", name: "Jerry" },
+      { type: "equipment", name: "Jerry's Computer", belongsTo: "Jerry" },
+    ],
+    [
+      //Office 202
+      { type: "person", name: "Sarah" },
+      { type: "equipment", name: "Sarah's Computer", belongsTo: "Sarah" },
+    ],
+  ],
+  [
+    //Third floor
+    [
+      //Office 301
+      { type: "person", name: "James" },
+      { type: "equipment", name: "James's Computer", belongsTo: "James" },
+    ],
+    [
+      //Office 302
+      { type: "person", name: "Erika" },
+      { type: "equipment", name: "Erika's Computer", belongsTo: "Erika" },
+    ],
+  ],
 ];
 
-const building = {
-  equipment: {
-    printer: ["Alice", "Fourth Floor", "Room 402"],
-    scanner: ["Patrick", "Third Floor", "Room 306"],
-    bendingMachine: ["Peter", "Second Floor", "Room 205"],
-    dataCenter: ["Sarah", "Third Floor", "Room 304"],
-    laptop: ["Jerry", "Second Floor", "Room 202"],
-  },
-  people: {
-    alice: ["Printer", "Fourth Floor", "Room 402"],
-    patrick: ["Scanner", "Third Floor", "Room 306"],
-    peter: ["Bending Machine", "Second Floor", "Room 205"],
-    sarah: ["Data Center", "Third Floor", "Room 304"],
-    jerry: ["laptop", "Second Floor", "Room 202"]
+let memory = [];
+function lookForEmployee(employee) {
+  if (memory.includes(employee)) {
+    return console.log("You already know where that person is");
   }
-};
-
-function buildGraph(edges) {
-  let graph = Object.create(null);
-  function addEdge(from, to) {
-    if (graph[from] == null) {
-      graph[from] = [to];
-    } else {
-      graph[from].push(to);
-    }
-  }
-  for (let [from, to] of edges.map((r) => r.split("-"))) {
-    addEdge(from, to);
-    addEdge(to, from);
-  }
-  return graph;
-}
-
-var roadGraph = buildGraph(roads);
-console.log("🚀 ~ file: Question02.js ~ line 35 ~ roadGraph", roadGraph);
-
-var VillageState = class VillageState {
-  constructor(place, parcels) {
-    this.place = place;
-    this.parcels = parcels;
-  }
-
-  move(destination) {
-    if (!roadGraph[this.place].includes(destination)) {
-      return this;
-    } else {
-      let parcels = this.parcels
-        .map((p) => {
-          if (p.place != this.place) return p;
-          return { place: destination, address: p.address };
-        })
-        .filter((p) => p.place != p.address);
-      return new VillageState(destination, parcels);
-    }
-  }
-};
-
-function runRobot(state, robot, memory) {
-  for (let turn = 0; ; turn++) {
-    if (state.parcels.length == 0) {
-      console.log(`Done in ${turn} turns`);
-      break;
-    }
-    let action = robot(state, memory);
-    state = state.move(action.direction);
-    memory = action.memory;
-    console.log(`Moved to ${action.direction}`);
-  }
-}
-
-function randomPick(array) {
-  let choice = Math.floor(Math.random() * array.length);
-  return array[choice];
-}
-
-function randomRobot(state) {
-  return { direction: randomPick(roadGraph[state.place]) };
-}
-
-VillageState.random = function (parcelCount = 5) {
-  let parcels = [];
-  for (let i = 0; i < parcelCount; i++) {
-    let address = randomPick(Object.keys(roadGraph));
-    let place;
-    do {
-      place = randomPick(Object.keys(roadGraph));
-    } while (place == address);
-    parcels.push({ place, address });
-  }
-  return new VillageState("Post Office", parcels);
-};
-
-runRobot(VillageState.random(), randomRobot);
-
-var mailRoute = [
-  "Alice's House",
-  "Cabin",
-  "Alice's House",
-  "Bob's House",
-  "Town Hall",
-  "Daria's House",
-  "Ernie's House",
-  "Grete's House",
-  "Shop",
-  "Grete's House",
-  "Farm",
-  "Marketplace",
-  "Post Office",
-];
-
-function routeRobot(state, memory) {
-  if (memory.length == 0) {
-    memory = mailRoute;
-  }
-  return { direction: memory[0], memory: memory.slice(1) };
-}
-
-runRobot(VillageState.random(), routeRobot, []);
-
-function findRoute(graph, from, to) {
-  let work = [{ at: from, route: [] }];
-  for (let i = 0; i < work.length; i++) {
-    let { at, route } = work[i];
-    for (let place of graph[at]) {
-      if (place == to) return route.concat(place);
-      if (!work.some((w) => w.at == place)) {
-        work.push({ at: place, route: route.concat(place) });
+  for (let floor = 0; floor < building.length; floor++) {
+    for (let office = 0; office < building[floor].length; office++) {
+      if (building[floor][office][0].name == employee) {
+        let address = building[floor][office][0].name;
+        console.log("🚀 ~ file: Question02.js ~ line 52 ~ lookForEmployee ~ address", address);
+        memory.push(address);
+        console.log(`${employee} was found in floor number ${[floor + 1]} in office ${floor + 1}0${office + 1}`);
+        console.log(memory);
       }
     }
   }
+  console.log(memory);
 }
 
-function goalOrientedRobot({ place, parcels }, route) {
-  if (route.length == 0) {
-    let parcel = parcels[0];
-    if (parcel.place != place) {
-      route = findRoute(roadGraph, place, parcel.place);
-    } else {
-      route = findRoute(roadGraph, place, parcel.address);
+function lookForEquipment(equipment) {
+  if (memory.includes(equipment)) {
+    return console.log("You already know where that equipment is");
+  }
+  for (let floor = 0; floor < building.length; floor++) {
+    for (let office = 0; office < building[floor].length; office++) {
+      if (building[floor][office][1].name == equipment) {
+        let address = building[floor][office][1].name;
+        console.log("🚀 ~ file: Question02.js ~ line 52 ~ lookForequipment ~ address", address);
+        memory.push(address);
+        console.log(`${equipment} was found in floor number ${[floor + 1]} in office ${floor + 1}0${office + 1}`);
+        console.log(memory);
+      }
     }
   }
-  return { direction: route[0], memory: route.slice(1) };
+  console.log(memory);
 }
 
-runRobot(VillageState.random(), goalOrientedRobot, []);
+lookForEmployee("Alice");
+lookForEmployee("Jerry");
+lookForEmployee("Erika");
+lookForEmployee("Erika");
+lookForEquipment("Sarah's Computer");
+lookForEquipment("Sarah's Computer");
+console.log(memory);
