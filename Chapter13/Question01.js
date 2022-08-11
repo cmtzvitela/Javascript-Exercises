52b4f885f4956be5b475bc1847f925c50d344050
unknown <cmtzvitela@gmail.com>
2022-08-11 16:30:27 -0500

    Second question from chapter 13

--- Chapter13/Question01.js
+++
@@ -1,4 +0,0 @@
-window.onclick = (e) => {
-  console.log(e.target);
-  alert(`You just clicked on number ${e.target.id}`);
-};
---
+++ Chapter13/Question02/Question02.js
@@ -0,0 +1,22 @@
+const button = document.querySelector("button");
+const button2 = document.getElementById("reset");
+const meter = document.querySelector("meter");
+let amountOfClicks = 0;
+
+button.addEventListener("click", (event) => {
+  amountOfClicks++;
+  meter.value = amountOfClicks;
+  if (amountOfClicks <= 2) {
+    button.textContent = `Click count: ${amountOfClicks}`;
+  } else if (amountOfClicks == 3) {
+    button.textContent = `Click count: ${amountOfClicks}`;
+    button.classList.add("offbutton");
+  }
+});
+
+button2.addEventListener("click", (event) => {
+  amountOfClicks = 0;
+  meter.value = 0;
+  button.textContent = `Click Me`;
+  button.classList.remove("offbutton");
+});
