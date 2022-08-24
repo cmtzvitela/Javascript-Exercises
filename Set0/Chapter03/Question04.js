61ea5c77370f3aa112b88dd26c5d5c68640095da
unknown <cmtzvitela@gmail.com>
2022-08-24 17:10:51 -0500

    Changes to Set 0, Chapter 3, Question 4

--- Set0/Chapter03/Question04.js
+++ Set0/Chapter03/Question04.js
@@ -1,26 +1,48 @@
-function Data(height, width) {
-  let newArray = [];
-  for (let i = 0; i < height; i++) {
-    newArray.push([]);
-    newArray[i].push(i);
-    for (let j = 0; j < width; j++) {
-      newArray[i][j] = j;
-    }
+function createPixelData(numberOfPixels) {
+  let pixelArray = [];
+  for (let i = 0; i < numberOfPixels; i++) {
+    pixelArray.push(colorArray[Math.floor(Math.random() * colorArray.length)]);
   }
-  console.log(newArray);
-  return newArray
+  return pixelArray;
 }
 
+const colorArray = ["yellow", "green", "red", "blue", "brown", "white", "black", "purple", "pink"];
+
+const data = createPixelData(400);
+console.log("🚀 ~ file: Question04.js ~ line 12 ~ data", data);
+
 function Image(data, height, width, name) {
-  this.pixelData = data;
+  this.pixelData = function getPixelData(indexX, indexY) {
+    if (indexX > this.width || indexY > this.height) {
+      return `Out of range. Max X = ${this.width} and max Y = ${this.height}`;
+    }
+    const index = indexX * this.width + indexY;
+    const pixelInfo = data[index];
+    return pixelInfo;
+  };
   this.height = height;
   this.width = width;
   this.name = name;
 }
-let data = new Data(40, 40);
-let img = new Image(data, 40, 40, "ImageName");
 
-console.log(img.pixelData[37][15]);
+let img = new Image(data, 20, 20, "ImageName");
+
+console.log(img.pixelData(0, 2));
 console.log(img.name);
 console.log(img.height);
 console.log(img.width);
+
+// function Data(height, width) {
+//   let newArray = [];
+//   for (let i = 0; i < height; i++) {
+//     newArray.push([]);
+//     newArray[i].push(i);
+//     for (let j = 0; j < width; j++) {
+//       newArray[i][j] = j;
+//     }
+//   }
+//   console.log(newArray);
+//   return newArray
+// }
+
+//let data = new Data(40, 40);
