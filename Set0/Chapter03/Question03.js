8ebbf34a32fe935e5d0e903a6f61ecde69d86c75
unknown <cmtzvitela@gmail.com>
2022-08-24 13:02:04 -0500

    The object now returns values in Question 3, Chapter3, set 0

--- Set0/Chapter03/Question03.js
+++ Set0/Chapter03/Question03.js
@@ -3,7 +3,7 @@ const myMath = {
     //console.log(args);
     const initialValue = 0;
     const sum = args.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
-    console.log(sum);
+    return sum;
   },
   mul: function mulNumbers(...args) {
     let initialValue = 1;
@@ -11,17 +11,17 @@ const myMath = {
       let result = initialValue * args[i];
       initialValue = result;
     }
-    console.log(initialValue);
+    return initialValue;
   },
   factorial: function factorial(num) {
     let f = 1;
     for (let i = num; i > 0; i--) {
       f = f * i;
     }
-    console.log(f);
+    return f;
   },
 };
 
-var a = myMath.add(1, 2, 3);
-var b = myMath.mul(8, 2, 3);
-var c = myMath.factorial(7);
+console.log(myMath.add(1, 2, 3));
+console.log(myMath.mul(8, 2, 3));
+console.log(myMath.factorial(7));
