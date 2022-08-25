79cc3ca9a83991bf9c1fcc0d49e0a78664e9b7b3
unknown <cmtzvitela@gmail.com>
2022-08-25 12:17:04 -0500

    Added Question 5, from Chapter 3, Set0

--- Set0/Chapter03/Question05.js
+++ Set0/Chapter03/Question05.js
@@ -1,28 +1,51 @@
-const obj = {
-  a: 45,
-  b: "A String",
-  c: true,
-  d: [1, 2, 3],
-  e: true,
-  f: false,
+function CustomObject(a, b) {
+  this.a = a;
+  this.b = b;
+}
+
+CustomObject.prototype.c = function () {
+  return this.a + this.b;
 };
 
-function objBoolKeys(object, boolean) {
-  if (object && boolean) {
-    let keyArray = Object.keys(object).filter(function (key) {
-      return object[key] === boolean;
-    });
-    console.log(keyArray);
-  } else if (object && !boolean) {
-    let keyArray = Object.keys(object).filter(function (key) {
-      return object[key] === boolean;
-    });
-    console.log(keyArray);
+const obj = new CustomObject(1, 2);
+
+function printObjProp(object, onlyBelongs) {
+  if (onlyBelongs == true) {
+    return Object.getOwnPropertyNames(object);
   } else {
-    console.log(Object.keys(obj));
+    return Object.getOwnPropertyNames(object).concat(Object.getOwnPropertyNames(object.__proto__));
   }
 }
 
-objBoolKeys(obj, true);
-objBoolKeys(obj, false);
-objBoolKeys(obj);
+console.log(printObjProp(obj));
+console.log(printObjProp(obj, false));
+console.log(printObjProp(obj, true));
+
+// const obj = {
+//   a: 45,
+//   b: "A String",
+//   c: true,
+//   d: [1, 2, 3],
+//   e: true,
+//   f: false,
+// };
+
+// function objBoolKeys(object, boolean) {
+//   if (object && boolean) {
+//     let keyArray = Object.keys(object).filter(function (key) {
+//       return object[key] === boolean;
+//     });
+//     return keyArray;
+//   } else if (object && !boolean) {
+//     let keyArray = Object.keys(object).filter(function (key) {
+//       return object[key] === boolean;
+//     });
+//     return keyArray;
+//   } else {
+//     return Object.keys(obj);
+//   }
+// }
+
+// objBoolKeys(obj, true);
+// objBoolKeys(obj, false);
+// objBoolKeys(obj);
