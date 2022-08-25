f491e07a401c7458594110c236002e8de4b5c393
unknown <cmtzvitela@gmail.com>
2022-08-25 12:59:12 -0500

    Made changes on Question 3, Chapter4, set0

--- Set0/Chapter04/Question03.js
+++ Set0/Chapter04/Question03.js
@@ -3,18 +3,18 @@ const numObj = {
   get number() {
     return this.num;
   },
-  set newNumber(num) {
+  set number(num) {
     if (typeof num === "number") {
       numObj.num = num;
     } else {
-      console.log("This is not a number, try again");
+      console.error("This is not a number, try again");
     }
   },
 };
 
-numObj.newNumber = "Hello";
-console.log(numObj.num);
-numObj.newNumber = [];
-console.log(numObj.num);
-numObj.newNumber = 8;
-console.log(numObj.num);
+numObj.number = "Hello";
+console.log(numObj.number);
+numObj.number = [];
+console.log(numObj.number);
+numObj.number = 8;
+console.log(numObj.number);
