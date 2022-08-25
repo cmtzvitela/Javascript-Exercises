61f6844ad663c3367c40e0191f048104e2419fac
unknown <cmtzvitela@gmail.com>
2022-08-25 18:08:00 -0500

    Changed the output of the function and made some changes in set0, chapter 7, question 2

--- Set0/Chapter07/Question02.js
+++ Set0/Chapter07/Question02.js
@@ -6,19 +6,23 @@ const germanHolidays = {
 
 function changeDateFormat(date) {
   let formattedDate = date.replace(/(\d{2})\/(\d{2})\/(\d{4})/, "$3/$1/$2");
-  console.log(`US date format: ${date}`);
-  console.log(`Germany date format: ${formattedDate}`);
-  let splittingDate = formattedDate.split("");
-  console.log("🚀 ~ file: Question02.js ~ line 8 ~ changeDateFormat ~ splittingDate", splittingDate);
-  let monthDay = splittingDate[5] + splittingDate[6] + splittingDate[7] + splittingDate[8] + splittingDate[9];
-  console.log("🚀 ~ file: Question02.js ~ line 10 ~ changeDateFormat ~ monthDay", monthDay);
-  console.log(typeof monthDay);
+  let monthDay = date.replace(/(\d{2})\/(\d{2})\/(\d{4})/, "$1/$2");
+  // console.log(`US date format: ${date}`);
+  // console.log(`Germany date format: ${formattedDate}`);
   if (monthDay in germanHolidays === false) {
     return;
   } else {
-    console.log("That's the German Holiday: ", germanHolidays[monthDay]);
+    return [
+      `US date format: ${date}`,
+      `Germany date format: ${formattedDate}`,
+      `That is the German Holiday: ${germanHolidays[monthDay]}`,
+    ];
   }
 }
 
 changeDateFormat("05/01/2025");
-changeDateFormat("10/03/2012");
+console.log(changeDateFormat("10/03/2012"));
+
+//let splittingDate = formattedDate.split("");
+//console.log("🚀 ~ file: Question02.js ~ line 8 ~ changeDateFormat ~ splittingDate", splittingDate);
+//let monthDay = splittingDate[5] + splittingDate[6] + splittingDate[7] + splittingDate[8] + splittingDate[9];
