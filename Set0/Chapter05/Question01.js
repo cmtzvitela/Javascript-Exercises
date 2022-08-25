26beaba831ecab1008a47a3c0a3d35589d1e2731
unknown <cmtzvitela@gmail.com>
2022-08-25 16:28:53 -0500

    The accountInfo function now works, replaced some logs with errors in question 1, chapter 5, set 0

--- Set0/Chapter05/Question01.js
+++ Set0/Chapter05/Question01.js
@@ -7,8 +7,8 @@ class Client {
       (this.balance = balance);
   }
   accountInfo(ID) {
-    if (ID == this.Client.accountID) {
-      const info = this.Client;
+    if (ID === this.accountID) {
+      const info = this;
       return info;
     }
   }
@@ -19,7 +19,7 @@ class Client {
     if (money <= this.ownMoney) {
       return (this.balance = money + this.balance);
     } else {
-      console.log("You didn't bring enough money to make that deposit");
+      console.error("You didn't bring enough money to make that deposit");
       return;
     }
   }
@@ -27,7 +27,7 @@ class Client {
     if (money <= this.balance) {
       return (this.balance = this.balance - money);
     } else {
-      console.log("Insufficient funds. Try another amount");
+      console.error("Insufficient funds. Try another amount");
       return;
     }
   }
@@ -40,7 +40,7 @@ class Client {
         return (receiver.balance = receiver.balance + money);
       }
     } else {
-      console.log("Insufficient funds. Try another amount");
+      console.error("Insufficient funds. Try another amount");
     }
   }
   depositAnotherfromBalance(money, sender, receiver) {
@@ -52,7 +52,7 @@ class Client {
         return (receiver.balance = receiver.balance + money);
       }
     } else {
-      console.log("Insufficient funds. Try another amount");
+      console.error("Insufficient funds. Try another amount");
     }
   }
 }
@@ -62,8 +62,10 @@ let y = 2500;
 const client1 = new Client("James", "client1", 12345, 3000, 1800);
 const client2 = new Client("Sarah", "client2", 98765, 5000, 2500);
 const client3 = new Client("Amelia", "client3", 78563, 1000, 6500);
+console.log(client1.viewBalance());
 console.log(client1.depositOwn(x));
 console.log(client1.withdraw(y));
+console.log(client1.accountInfo(12345));
 
 const bank = {
   client1: client1,
