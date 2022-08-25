e70da9f58c51088ad861d995f171dc5f7fd71080
unknown <cmtzvitela@gmail.com>
2022-08-25 17:37:33 -0500

    Modified the function in question 1, chapter 7, set 0

--- Set0/Chapter07/Question01.js
+++ Set0/Chapter07/Question01.js
@@ -1,11 +1,18 @@
 function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
-  }
-  
-console.log(`${hexToRgb("#0033ff").r}, ${hexToRgb("#0033ff").g}, ${hexToRgb("#0033ff").b}`);
-console.log(`${hexToRgb("#482b7d").r}, ${hexToRgb("#482b7d").g}, ${hexToRgb("#482b7d").b}`);
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  let rgb = `rgb ${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`;
+  return rgb;
+}
+
+console.log(hexToRgb("#0033ff"));
+console.log(hexToRgb("#482b7d"));
+
+// return result
+//   ? {
+//       r: parseInt(result[1], 16),
+//       g: parseInt(result[2], 16),
+//       b: parseInt(result[3], 16),
+//     }
+//   : null;
+// console.log(`rgb (${hexToRgb("#0033ff").r}, ${hexToRgb("#0033ff").g}, ${hexToRgb("#0033ff").b})`);
+// console.log(`rgb (${hexToRgb("#482b7d").r}, ${hexToRgb("#482b7d").g}, ${hexToRgb("#482b7d").b})`);
