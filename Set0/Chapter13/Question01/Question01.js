bfb88c1b4fefb6e7c1467600548c00c66b2ea9e6
unknown <cmtzvitela@gmail.com>
2022-08-26 16:42:54 -0500

    Slight changes to the function and event delegation properly set on element on set0, chapter 13, question 1

--- Set0/Chapter13/Question01/Question01.js
+++ Set0/Chapter13/Question01/Question01.js
@@ -1,13 +1,9 @@
-addEventListener("click", (event) => {
-  const tdClick = document.getElementById(event.target.id);
-  //console.log("🚀 ~ file: Question01.js ~ line 3 ~ addEventListener ~ tdClick", tdClick);
+const table = document.getElementsByTagName("table")[0];
 
-  let tdValue = tdClick.getAttribute("value");
-  //console.log("🚀 ~ file: Question01.js ~ line 6 ~ addEventListener ~ tdValue", tdValue);
+table.addEventListener("click", (event) => {
+  let tdValue = event.target.id;
   alert(`You clicked on number ${tdValue}`);
 });
 
-// window.onclick = (e) => {
-//   console.log(e.target); // to get the element
-//   alert(`You just clicked on number ${e.target.id}`); // to get the element tag name alone
-// };
+// const tdClick = document.getElementById(event.target.id);
+// let tdValue = tdClick.getAttribute("value");
