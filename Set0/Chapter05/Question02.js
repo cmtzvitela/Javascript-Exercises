355f2152b397fabf4ac57dc1fdac468d12d90808
unknown <cmtzvitela@gmail.com>
2022-08-30 17:08:18 -0500

    Modified Question 5.2 in Set#0

--- Set0/Chapter05/Question02.js
+++ Set0/Chapter05/Question02.js
@@ -41,46 +41,37 @@ const building = [
 ];
 
 let memory = [];
-function lookForEmployee(employee) {
-  if (memory.includes(employee)) {
-    return console.log("You already know where that person is");
-  }
-  for (let floor = 0; floor < building.length; floor++) {
-    for (let office = 0; office < building[floor].length; office++) {
-      if (building[floor][office][0].name == employee) {
-        let address = building[floor][office][0].name;
-        console.log("🚀 ~ file: Question02.js ~ line 52 ~ lookForEmployee ~ address", address);
-        memory.push(address);
-        console.log(`${employee} was found in floor number ${[floor + 1]} in office ${floor + 1}0${office + 1}`);
-        console.log(memory);
+function search(searchParameter) {
+  const inMemory = memory.filter((registered) => registered.includes(searchParameter));
+  if (inMemory.length == 0) {
+    for (let floor = 0; floor < building.length; floor++) {
+      for (let office = 0; office < building[floor].length; office++) {
+        if (building[floor][office][0].name == searchParameter) {
+          memory.push(
+            `${searchParameter} was found in floor number ${[floor + 1]} in office ${floor + 1}0${office + 1}`
+          );
+        } else if (building[floor][office][1].name == searchParameter) {
+          memory.push(
+            `${searchParameter} was found in floor number ${[floor + 1]} in office ${floor + 1}0${office + 1}`
+          );
+        }
       }
     }
-  }
-  console.log(memory);
-}
-
-function lookForEquipment(equipment) {
-  if (memory.includes(equipment)) {
-    return console.log("You already know where that equipment is");
-  }
-  for (let floor = 0; floor < building.length; floor++) {
-    for (let office = 0; office < building[floor].length; office++) {
-      if (building[floor][office][1].name == equipment) {
-        let address = building[floor][office][1].name;
-        console.log("🚀 ~ file: Question02.js ~ line 52 ~ lookForequipment ~ address", address);
-        memory.push(address);
-        console.log(`${equipment} was found in floor number ${[floor + 1]} in office ${floor + 1}0${office + 1}`);
-        console.log(memory);
+  } else {
+    memory.forEach((element) => {
+      if (element.includes(searchParameter)) {
+        console.log(element);
+        return element;
       }
-    }
+    });
   }
-  console.log(memory);
 }
 
-lookForEmployee("Alice");
-lookForEmployee("Jerry");
-lookForEmployee("Erika");
-lookForEmployee("Erika");
-lookForEquipment("Sarah's Computer");
-lookForEquipment("Sarah's Computer");
+search("Alice");
+search("Jerry");
+search("Erika");
 console.log(memory);
+search("Erika");
+search("Sarah's Computer");
+console.log(memory);
+search("Sarah's Computer");
