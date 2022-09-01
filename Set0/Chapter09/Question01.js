3d5a0efce32395080d5d5350dce6cc6337e3cf42
unknown <cmtzvitela@gmail.com>
2022-09-01 13:43:49 -0500

    Modifications to Question 9 and 14.3 of Set#0

--- Set0/Chapter09/Question01.js
+++ Set0/Chapter09/Question01.js
@@ -1,38 +1,11 @@
-var str = "{prop1: 42, myFn: function(a, b) { return a+b+this.prop1;}}";
-var str2 = {
-  prop1: 42,
-  myFn: function (a, b) {
-    return a + b + this.prop1;
-  },
-};
-//console.log(eval(str));
-//const evalu = new Function(str);
-//console.log(evalu(5, 4));
-str = str.slice(0, str.length - 1);
-console.log(str);
-str;
-function dataParse(str) {
-  const replacedStr = str.replace(/['"]+/g, "");
-  return replacedStr;
+const str = "{prop1: 42, myFn: function(a, b) { return a+b+this.prop1;}}";
+
+function evalString(string) {
+  return function (...arg) {
+    const obj = eval("(" + string + ")");
+    return obj.myFn(...arg);
+  };
 }
 
-function copy(x) {
-  return JSON.parse(JSON.stringify(x));
-}
-let otherObj = copy(str);
-console.log(otherObj);
-
-otherObj.myFn();
-// str = str.replace(/^"(.*)"$/, '$1');
-// console.log(str)
-// const anotherObj = {}
-// console.log(anotherObj)
-const weirdObj = {
-  prop1: 42,
-  myFn: function (a, b) {
-    return a + b + this.prop1;
-  },
-};
-dataParse(str);
-console.log(eval(weirdObj.myFn(5, 4)));
-console.log(weirdObj.myFn(5, 4));
+const evaluatedString = evalString(str);
+console.log(evaluatedString(3, 4));
--- Set0/Chapter14/Question03/Question03.js
+++ Set0/Chapter14/Question03/Question03.js
@@ -1,98 +1,160 @@
-import info from "./info.json" assert { type: "json" };
-const emptyDiv = document.getElementById("empty-div");
+fetch("./info.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((info) => {
+    const defaultTemplate = document.createDocumentFragment();
+    const homeAnchor = document.createElement("a");
+    homeAnchor.id = "home";
+    const container = document.createElement("section");
+    const imageContainer = document.createElement("div");
+    imageContainer.classList = "image-container";
+    container.appendChild(imageContainer);
+    homeAnchor.appendChild(container);
+    createArticles();
+    defaultTemplate.appendChild(homeAnchor);
+    document.body.appendChild(defaultTemplate);
 
-const template1 = document.getElementById("template-1");
-const template1Clone = template1.content.firstElementChild.cloneNode(true);
-emptyDiv.appendChild(template1Clone);
+    function createArticles() {
+      for (let i = 0; i < info.length; i++) {
+        const anchor = document.createElement("a");
+        anchor.href = `#article${info[i].id}`;
+        const content = document.createElement("div");
+        anchor.appendChild(content);
+        content.className = "content";
+        content.id = `article ${info[i].id}`;
+        const image = document.createElement("div");
+        image.className = "image";
+        const numberImage = document.createElement("h3");
+        image.appendChild(numberImage);
+        content.appendChild(image);
+        image.style.backgroundImage = "url(" + info[i].img + ")";
+        const article = document.createElement("article");
+        content.appendChild(article);
+        const title = document.createElement("h2");
+        title.id = "title";
+        title.textContent = info[i].title;
+        article.appendChild(title);
+        const text = document.createElement("p");
+        text.id = "text";
+        text.textContent = info[i].text;
+        article.appendChild(text);
+        imageContainer.appendChild(anchor);
+        content.addEventListener("click", (event) => {
+          return otherTemplate(info[i]);
+        });
+      }
+    }
 
-//template1Clone.addEventListener("click", firstTemplateClick);
+    function otherTemplate(obj) {
+      const secondTemplate = document.createDocumentFragment();
+      const divContainer = document.createElement("div");
+      divContainer.id = `article${obj.id}`;
+      divContainer.classList = "second-template-container";
+      secondTemplate.appendChild(divContainer);
+      const title = document.createElement("h2");
+      divContainer.appendChild(title);
+      title.textContent = obj.title;
+      const imageDiv = document.createElement("div");
+      divContainer.appendChild(imageDiv);
+      const image = document.createElement("img");
+      image.src = obj.img;
+      image.classList = "article-image";
+      imageDiv.appendChild(image);
+      const text = document.createElement("p");
+      text.textContent = obj.text;
+      divContainer.appendChild(text);
+      const goBack = document.createElement("a");
+      goBack.textContent = "Go back!";
+      goBack.href = "#home";
+      divContainer.appendChild(goBack);
+      document.body.removeChild(homeAnchor);
+      document.body.appendChild(secondTemplate);
+      goBack.addEventListener("click", () => {
+        document.body.removeChild(divContainer);
+        firstTemplate();
+      });
+    }
 
-const template2 = document.getElementById("template-2");
-const template2Clone = template2.content.firstElementChild.cloneNode(true);
+    function firstTemplate() {
+      document.body.appendChild(homeAnchor);
+    }
+  });
 
-const firstTitle = document.getElementById("first-title");
-firstTitle.textContent = info.article1.title;
-const firstText = document.getElementById("first-text");
-firstText.textContent = info.article1.text;
+// import info from "./info.json" assert { type: "json" };
+// const defaultTemplate = document.createDocumentFragment();
+// const homeAnchor = document.createElement("a");
+// homeAnchor.id = "home";
+// const container = document.createElement("section");
+// const imageContainer = document.createElement("div");
+// imageContainer.classList = "image-container";
+// container.appendChild(imageContainer);
+// homeAnchor.appendChild(container);
+// createArticles();
+// defaultTemplate.appendChild(homeAnchor);
+// document.body.appendChild(defaultTemplate);
 
-const secondTitle = document.getElementById("second-title");
-secondTitle.textContent = info.article2.title;
-const secondText = document.getElementById("second-text");
-secondText.textContent = info.article2.text;
-
-const thirdTitle = document.getElementById("third-title");
-thirdTitle.textContent = info.article3.title;
-const thirdText = document.getElementById("third-text");
-thirdText.textContent = info.article3.text;
-
-const contentClick = template1Clone.getElementsByClassName("content");
-
-contentClick[0].addEventListener("click", firstTemplateClick);
-contentClick[1].addEventListener("click", firstTemplateClick);
-contentClick[2].addEventListener("click", firstTemplateClick);
-
-function firstTemplateClick(event) {
-  const imageContainer = document.getElementsByClassName("image");
-  switch (event.target) {
-    case imageContainer[0]:
-      console.log(event.target);
-      const parentId1 = event.target.parentNode.id;
-      console.log("🚀 ~ file: Question03.js ~ line 40 ~ firstTemplateClick ~ parentId", parentId1);
-
-      createSecondTemplate(parentId1);
-      break;
-    case imageContainer[1]:
-      console.log(event.target);
-      const parentId2 = event.target.parentNode.id;
-      console.log("🚀 ~ file: Question03.js ~ line 40 ~ firstTemplateClick ~ parentId", parentId2);
-
-      createSecondTemplate(parentId2);
-      break;
-    case imageContainer[2]:
-      console.log(event.target);
-      const parentId3 = event.target.parentNode.id;
-      console.log("🚀 ~ file: Question03.js ~ line 40 ~ firstTemplateClick ~ parentId", parentId3);
-
-      createSecondTemplate(parentId3);
-      break;
-
-    default:
-      const parentId = event.target.parentNode.parentNode.id;
-      console.log("🚀 ~ file: Question03.js ~ line 44 ~ firstTemplateClick ~ parentId", parentId);
-
-      createSecondTemplate(parentId);
-      break;
-  }
-}
-
-function secondTemplateClick() {
-  emptyDiv.removeChild(template2Clone);
-  emptyDiv.appendChild(template1Clone);
-}
-
-function createSecondTemplate(parentId) {
-  emptyDiv.removeChild(template1Clone);
-  emptyDiv.appendChild(template2Clone);
-  const individualTitle = document.getElementById("individual-title");
-  individualTitle.textContent = info[parentId].title;
-  const firstText = document.getElementById("article-text");
-  firstText.textContent = info[parentId].text;
-  const articleImage = document.getElementById("article-image");
-  articleImage.src = info[parentId].img;
-  const backLink = document.getElementById("go-back");
-  backLink.addEventListener("click", secondTemplateClick);
-}
-
-//   console.log("🚀 ~ file: Question03.js ~ line 37 ~ firstTemplateClick ~ imageContainer", imageContainer);
-//   if (event.target == imageContainer[0]) {
-//     console.log(event.target);
-//     const parentId = event.target.parentNode.id;
-//     console.log("🚀 ~ file: Question03.js ~ line 40 ~ firstTemplateClick ~ parentId", parentId);
-
-//     createSecondTemplate(parentId);
-//     return;
+// function createArticles() {
+//   for (let i = 0; i < info.length; i++) {
+//     const anchor = document.createElement("a");
+//     anchor.href = `#article${info[i].id}`;
+//     const content = document.createElement("div");
+//     anchor.appendChild(content);
+//     content.className = "content";
+//     content.id = `article ${info[i].id}`;
+//     const image = document.createElement("div");
+//     image.className = "image";
+//     const numberImage = document.createElement("h3");
+//     image.appendChild(numberImage);
+//     content.appendChild(image);
+//     image.style.backgroundImage = "url(" + info[i].img + ")";
+//     const article = document.createElement("article");
+//     content.appendChild(article);
+//     const title = document.createElement("h2");
+//     title.id = "title";
+//     title.textContent = info[i].title;
+//     article.appendChild(title);
+//     const text = document.createElement("p");
+//     text.id = "text";
+//     text.textContent = info[i].text;
+//     article.appendChild(text);
+//     imageContainer.appendChild(anchor);
+//     content.addEventListener("click", (event) => {
+//       return otherTemplate(info[i]);
+//     });
 //   }
-//   const parentId = event.target.parentNode.parentNode.id;
-//   console.log("🚀 ~ file: Question03.js ~ line 44 ~ firstTemplateClick ~ parentId", parentId);
+// }
 
-//   createSecondTemplate(parentId);
+// function otherTemplate(obj) {
+//   const secondTemplate = document.createDocumentFragment();
+//   const divContainer = document.createElement("div");
+//   divContainer.id = `article${obj.id}`;
+//   divContainer.classList = "second-template-container";
+//   secondTemplate.appendChild(divContainer);
+//   const title = document.createElement("h2");
+//   divContainer.appendChild(title);
+//   title.textContent = obj.title;
+//   const imageDiv = document.createElement("div");
+//   divContainer.appendChild(imageDiv);
+//   const image = document.createElement("img");
+//   image.src = obj.img;
+//   image.classList = "article-image";
+//   imageDiv.appendChild(image);
+//   const text = document.createElement("p");
+//   text.textContent = obj.text;
+//   divContainer.appendChild(text);
+//   const goBack = document.createElement("a");
+//   goBack.textContent = "Go back!";
+//   goBack.href = "#home";
+//   divContainer.appendChild(goBack);
+//   document.body.removeChild(homeAnchor);
+//   document.body.appendChild(secondTemplate);
+//   goBack.addEventListener("click", () => {
+//     document.body.removeChild(divContainer);
+//     firstTemplate();
+//   });
+// }
+
+// function firstTemplate() {
+//   document.body.appendChild(homeAnchor);
+// }
