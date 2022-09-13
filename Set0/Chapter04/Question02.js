d106b4bd616a31c7436439cd32d6618ee10a6b68
unknown <cmtzvitela@gmail.com>
2022-09-13 14:05:43 -0500

    Changed 4.2 Set#0 to not be destructive

--- Set0/Chapter04/Question02.js
+++ Set0/Chapter04/Question02.js
@@ -1,11 +1,12 @@
 function addRec(arr) {
-  if (arr.length == 0) {
-    return 0;
+  if (arr.length === 1) {
+    return arr[0];
   } else {
-    return arr.shift() + addRec(arr);
+    return arr[arr.length - 1] + addRec(arr.slice(0, arr.length - 1));
   }
 }
 
 let numberArray = [1, 4, 6, 7, 48];
 let sum = addRec(numberArray);
+console.log("🚀 ~ file: Question02.js ~ line 11 ~ numberArray", numberArray);
 console.log("🚀 ~ file: Question02.js ~ line 11 ~ sum", sum);
