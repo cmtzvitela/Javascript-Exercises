c6562ce116e0b223737d533ef322da41f5131974
unknown <cmtzvitela@gmail.com>
2022-09-15 17:08:42 -0500

    Another solution for question 3C of Set#2

--- Set2/Question03/Question03-C.js
+++ Set2/Question03/Question03-C.js
@@ -1,10 +1,14 @@
 const palindrome = "_@12345!54321@_";
 
-function checkPalindrome(string) {
-  let tempString = string.match(/.+/gi).join("").toLowerCase();
-  let secondString = tempString.split("").reverse().join("");
-  return tempString === secondString;
-}
+const palindromeFinder = /\b(.?)(.?)(.?)(.?)(.?)(.?)(.?)(.?)(.?)(.)\S?\10\9\8\7\6\5\4\3\2\1\b/g;
 
-console.log(checkPalindrome(palindrome));
-//console.log(palindrome.match(/^(.)\w+\1$/));
+console.log(palindrome.match(palindromeFinder));
+
+// function checkPalindrome(string) {
+//   let tempString = string.match(/.+/gi).join("").toLowerCase();
+//   let secondString = tempString.split("").reverse().join("");
+//   return tempString === secondString;
+// }
+
+// console.log(checkPalindrome(palindrome));
+// console.log(palindrome.match(/^(.)\w+\1$/));
