03b4d137a89d78799a9656d58fc1aaa7e07fe44b
unknown <cmtzvitela@gmail.com>
2022-09-19 13:00:54 -0500

    Changed the if statement in Question 1 Set#1

--- Set1/Question01/Question01.js
+++ Set1/Question01/Question01.js
@@ -75,19 +75,7 @@ function consonants(str) {
   var countConsonants = 0;
 
   for (var i = 0; i < str.length; i++) {
-    if (
-      str[i] !== "a" &&
-      str[i] !== "e" &&
-      str[i] !== "i" &&
-      str[i] !== "o" &&
-      str[i] !== "u" &&
-      str[i] !== " " &&
-      str[i] !== "A" &&
-      str[i] !== "E" &&
-      str[i] !== "I" &&
-      str[i] !== "O" &&
-      str[i] !== "U"
-    ) {
+    if (!"aeiouAEIOU".includes(str[i])) {
       countConsonants++;
     }
   }
