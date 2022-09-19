b6daee782dc2ba680ae2d83087e186614a5855d0
unknown <cmtzvitela@gmail.com>
2022-09-19 16:44:17 -0500

    Minor changes

--- Set1/Question03/Question03.js
+++ Set1/Question03/Question03.js
@@ -1,8 +1,9 @@
-
 const bigTitle = document.getElementById("big-title-change");
 const bigTitle2 = document.getElementById("big-title-default");
 
-bigTitle.addEventListener("click", changeH1.bind(bigTitle, "blue", "96px", "red"));
+bigTitle.addEventListener("click", function () {
+  changeH1.call(this, "blue", "96px", "red");
+});
 bigTitle2.addEventListener("click", changeH1.bind(bigTitle2));
 
 function changeH1(color = "green", fontSize = "12px", backgroundColor = "yellow") {
@@ -10,4 +11,3 @@ function changeH1(color = "green", fontSize = "12px", backgroundColor = "yellow"
   this.style.fontSize = fontSize;
   this.style.backgroundColor = backgroundColor;
 }
-
