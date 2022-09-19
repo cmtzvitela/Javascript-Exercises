1526cdf3f9c3938fc9f3d7f6613de15f841698a4
unknown <cmtzvitela@gmail.com>
2022-09-19 16:29:04 -0500

    Changed question 3 of set#1

--- Set1/Question03/Question03.js
+++ Set1/Question03/Question03.js
@@ -1,26 +1,13 @@
-function highlight(elem, color, fontSize, backgroundColor) {
-  elem.style.color = "green";
-  elem.style.fontSize = "12px";
-  elem.style.backgroundColor = "yellow";
-  elem.style.color = color;
-  elem.style.fontSize = fontSize;
-  elem.style.backgroundColor = backgroundColor;
+
+const bigTitle = document.getElementById("big-title-change");
+const bigTitle2 = document.getElementById("big-title-default");
+
+bigTitle.addEventListener("click", changeH1.bind(bigTitle, "blue", "96px", "red"));
+bigTitle2.addEventListener("click", changeH1.bind(bigTitle2));
+
+function changeH1(color = "green", fontSize = "12px", backgroundColor = "yellow") {
+  this.style.color = color;
+  this.style.fontSize = fontSize;
+  this.style.backgroundColor = backgroundColor;
 }
 
-
-// const bigTitle = document.getElementById("big-title");
-// console.log("🚀 ~ file: Question03.js ~ line 2 ~ bigTitle", bigTitle);
-
-// const boundedTitle = changeH1.bind(bigTitle);
-
-// boundedTitle.addEventListener("click", changeH1("blue", "96px", "yellow"));
-
-//function changeH1(color, fontSize, backgroundColor) {
-// this.style.color = "green";
-// this.style.fontSize = "12px";
-// this.style.backgroundColor = "red";
-
-//   this.style.color = color;
-//   this.style.fontSize = fontSize;
-//   this.style.backgroundColor = backgroundColor;
-// }
