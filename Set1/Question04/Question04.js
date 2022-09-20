a418ebb36ee3f8125d67da8a9df9a68b5dd275d9
unknown <cmtzvitela@gmail.com>
2022-09-20 14:21:31 -0500

    Changed Question 4 in Set#1

--- Set1/Question04/Question04.js
+++ Set1/Question04/Question04.js
@@ -11,33 +11,25 @@ const objB = {
 
 function copy(obj1, obj2, propertyArray) {
   if (obj1 && obj2 && propertyArray) {
-    //If an array of properties is provided, it will reduce the object to only have only the asked components
-    return Object.keys(obj2).reduce((target, k) => {
-      if (propertyArray.indexOf(k) > -1) {
-        target[k] = obj2[k];
+    const keyArray = Object.keys(obj2);
+    for (let i = 0; i < keyArray.length; i++) {
+      if (propertyArray.includes(keyArray[i])) {
+        obj1[keyArray[i]] = obj2[keyArray[i]];
       }
-      //And then assing them to the other object
-      return Object.assign(target, obj1);
-    }, {});
+    }
+    return obj1;
+    //If an array of properties is provided, it will reduce the object to have only the asked components
+    // return Object.keys(obj2).reduce((target, k) => {
+    //   if (propertyArray.indexOf(k) > -1) {
+    //     target[k] = obj2[k];
+    //   }
+    //   //And then assing them to the other object
+    //   return Object.assign(target, obj1);
+    // }, {});
   } else if (obj1 && obj2) {
     //If an array is not provided, it will just put the objects together
-    const obj = Object.assign(obj1, obj2);
-    return obj;
+    return Object.assign(obj1, obj2);
   }
 }
 console.log(copy(objA, objB, ["occupation"]));
 console.log(copy(objA, objB));
-
-// function copyObj(obj1, obj2, propertyArray) {
-//   if (obj1 && obj2 && propertyArray) {
-//     let res = Object.keys(obj2).reduce(function (obj, k) {
-//       if ((k = propertyArray)) obj[k] = obj2[k];
-//       return obj;
-//     }, {});
-//     const obj = Object.assign(obj1, res);
-//     return obj;
-//   } else if (obj1 && obj2) {
-//     const obj = Object.assign(obj1, obj2);
-//     return obj;
-//   }
-// }
