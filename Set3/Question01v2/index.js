a9e6e72c479d9ea8f132ee08fe2fdd2e8d68c8c3
unknown <cmtzvitela@gmail.com>
2022-09-26 14:57:00 -0500

    New attempt at Set#3

---
+++ Set3/Question01v2/index.js
@@ -0,0 +1,120 @@
+const noteFunctions = {
+  getAllNotes() {
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+    return notes.sort((a, b) => {
+      return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+    });
+  },
+
+  displayNotes() {
+    const notes = this.getAllNotes();
+    notes.forEach((note) => {
+      const noteTemplate = document.getElementById("new-note");
+      const clonedTemplate = noteTemplate.content.cloneNode(true);
+      const noteDiv = clonedTemplate.getElementById("note-body");
+      noteDiv.id = `${note.id}`;
+      const noteSection = document.getElementById("note-space");
+      const noteTitle = clonedTemplate.getElementById("note-title");
+      noteTitle.value = `${note.title}`;
+      const noteText = clonedTemplate.getElementById("note-text");
+      noteText.value = `${note.body}`;
+      const noteToUpdate = clonedTemplate.getElementById(`${note.id}`);
+      const span = noteToUpdate.getElementsByTagName("span");
+      const updatedDate = note.updated ? new Date(note.updated) : new Date(note.creationDate);
+      span[0].textContent = `Last updated ${updatedDate.toLocaleString()}`;
+      noteSection.appendChild(clonedTemplate);
+    });
+  },
+
+  deleteNote(noteID) {
+    const notes = this.getAllNotes();
+    const newNotes = notes.filter((note) => note.id != noteID);
+
+    localStorage.setItem("notes", JSON.stringify(newNotes));
+    const noteSection = document.getElementById("note-space");
+    while (noteSection.firstChild) {
+      noteSection.removeChild(noteSection.firstChild);
+    }
+    this.displayNotes();
+    this.addTrash();
+  },
+  updatedDate(existingNote) {
+    const noteToUpdate = document.getElementById(existingNote.id);
+    const span = noteToUpdate.getElementsByTagName("span");
+    const date = existingNote.updated;
+    span[0].textContent = `Last updated ${date.toLocaleString()}`;
+  },
+
+  editNote(noteToEdit) {
+    const notes = noteFunctions.getAllNotes();
+    const existing = notes.find((note) => note.id == noteToEdit.id);
+    if (existing) {
+      existing.title = noteToEdit.title;
+      existing.body = noteToEdit.body;
+      existing.updated = new Date();
+      this.updatedDate(existing);
+    }
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+  },
+
+  blurNote() {
+    const inpTitle = document.getElementsByClassName("note-title");
+    const inpBody = document.getElementsByClassName("note-text");
+    for (let i = 0; i < inpTitle.length; i++) {
+      inpTitle[i].addEventListener("blur", (event) => {
+        const updatedTitle = inpTitle[i].value;
+        const updatedBody = inpBody[i].value;
+        const noteID = event.currentTarget.parentNode.id;
+        this.editNote({ id: noteID, title: updatedTitle, body: updatedBody });
+      });
+      inpBody[i].addEventListener("blur", (event) => {
+        const updatedTitle = inpTitle[i].value;
+        const updatedBody = inpBody[i].value;
+        const noteID = event.currentTarget.parentNode.id;
+        this.editNote({ id: noteID, title: updatedTitle, body: updatedBody });
+      });
+    }
+  },
+
+  createNote() {
+    const newNote = {
+      id: Math.floor(Math.random() * 1000000),
+      title: "New Note",
+      body: "Take note...",
+      creationDate: new Date(),
+    };
+    if (!localStorage.getItem("notes")) {
+      localStorage.setItem("notes", JSON.stringify([]));
+    }
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    notes.push(newNote);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    const noteTemplate = document.getElementById("new-note");
+    const clonedTemplate = noteTemplate.content.cloneNode(true);
+    const noteSection = document.getElementById("note-space");
+    noteSection.appendChild(clonedTemplate);
+    this.addTrash();
+    this.blurNote();
+  },
+
+  addTrash() {
+    const trashButton = document.getElementsByClassName("note-delete");
+
+    for (let i = 0; i < trashButton.length; i++) {
+      trashButton[i].addEventListener("click", (event) => {
+        noteFunctions.deleteNote(event.currentTarget.parentNode.id);
+      });
+    }
+  },
+};
+
+noteFunctions.displayNotes();
+const createNoteButton = document.getElementById("create-note-button");
+createNoteButton.addEventListener("click", () => {
+  noteFunctions.createNote();
+});
+
+noteFunctions.addTrash();
+noteFunctions.blurNote();
