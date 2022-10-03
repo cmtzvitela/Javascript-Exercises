af89b5071b01f5f737c5df3b0f8910cceb5e016f
unknown <cmtzvitela@gmail.com>
2022-10-03 18:16:48 -0500

    Added drag and drop to notes

--- Patterns/App.js
+++ Patterns/App.js
@@ -1,5 +1,5 @@
 import { getAllNotes, searchNote } from "./noteFunctions.js";
-import { displayNotes, blurNote, createNote, addTrash } from "./noteView.js";
+import { displayNotes, blurNote, createNote, addTrash, dragNote, dropNote } from "./noteView.js";
 
 const initialNotes = getAllNotes();
 displayNotes(initialNotes);
@@ -17,3 +17,10 @@ searchInput.addEventListener("input", (e) => {
 
 addTrash();
 blurNote();
+dragNote();
+dropNote();
+
+const noteSection = document.getElementById("note-space")
+let sortable = new Sortable(noteSection, {
+  
+})
