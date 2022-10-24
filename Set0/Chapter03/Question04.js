b884721e08f3e4c72cc592c55862ea6fffc5b474
unknown <cmtzvitela@gmail.com>
2022-10-24 14:33:27 -0500

    Changed the console.error into throwing an error

--- Set0/Chapter03/Question04.js
+++ Set0/Chapter03/Question04.js
@@ -13,7 +13,7 @@ const data = createPixelData(400);
 function Image(data, height, width, name) {
   this.pixelData = function getPixelData(indexX, indexY) {
     if (indexX > this.width || indexY > this.height) {
-      return console.error(`Out of range. Max X = ${this.width} and max Y = ${this.height}`);
+      throw new Error(`Out of range. Max X = ${this.width} and max Y = ${this.height}`);
     }
     const index = indexX * this.width + indexY;
     const pixelInfo = data[index];
@@ -30,18 +30,3 @@ console.log(img.pixelData(0, 2));
 console.log(img.name);
 console.log(img.height);
 console.log(img.width);
-
-// function Data(height, width) {
-//   let newArray = [];
-//   for (let i = 0; i < height; i++) {
-//     newArray.push([]);
-//     newArray[i].push(i);
-//     for (let j = 0; j < width; j++) {
-//       newArray[i][j] = j;
-//     }
-//   }
-//   console.log(newArray);
-//   return newArray
-// }
-
-//let data = new Data(40, 40);
