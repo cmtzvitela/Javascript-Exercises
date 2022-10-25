50ca69fdfd1f58b5fcdb59ccf0de8f790f3e2a10
unknown <cmtzvitela@gmail.com>
2022-10-25 14:49:29 -0500

    Simplified the code of 8.2

--- Set0/Chapter08/Question02.js
+++ Set0/Chapter08/Question02.js
@@ -17,60 +17,16 @@ function printSentences() {
   let counter = 1;
   return function incrementCounter() {
     counter++;
-    if (counter % 2 === 0 && counter % 4 === 0 && counter % 5 === 0) {
+    if (counter % 2 === 0) {
       thisIsA();
+    }
+    if (counter % 4 === 0) {
       thisIsB();
-      thisIsC();
-    } else if (counter % 2 === 0 && counter % 4 === 0) {
-      thisIsA();
-      thisIsB();
-    } else if (counter % 2 === 0 && counter % 5 === 0) {
-      thisIsA();
-      thisIsC();
-    } else if (counter % 4 === 0 && counter % 5 === 0) {
-      thisIsB();
-      thisIsC();
-    } else if (counter % 2 === 0) {
-      thisIsA();
-    } else if (counter % 4 === 0) {
-      thisIsB();
-    } else if (counter % 5 === 0) {
+    }
+    if (counter % 5 === 0) {
       thisIsC();
     }
     console.log(counter);
     return counter;
   };
 }
-
-// const start = Date.now();
-// console.log("The timer has started");
-
-// setInterval(printSentences, 1000);
-
-// function printSentences() {
-//   const timeElapsed = Date.now() - start;
-//   if (timeElapsed % 30000 < 999 && timeElapsed % 60000 < 999 && timeElapsed % 75000 < 999) {
-//     thisIsA();
-//     thisIsB();
-//     thisIsC();
-//     return;
-//   } else if (timeElapsed % 30000 < 999 && timeElapsed % 60000 < 999) {
-//     thisIsA();
-//     thisIsB();
-//     return;
-//   } else if (timeElapsed % 30000 < 999 && timeElapsed % 75000 < 999) {
-//     thisIsA();
-//     thisIsC();
-//     return;
-//   } else if (timeElapsed % 60000 < 999 && timeElapsed % 75000 < 999) {
-//     thisIsB();
-//     thisIsC();
-//     return;
-//   } else if (timeElapsed % 30000 < 999) {
-//     return thisIsA();
-//   } else if (timeElapsed % 60000 < 999) {
-//     return thisIsB();
-//   } else if (timeElapsed % 75000 < 999) {
-//     return thisIsC();
-//   }
-// }
