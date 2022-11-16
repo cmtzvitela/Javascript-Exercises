5355f639ae57e9c0ba5eb073d0cb61dc682f7abc
unknown <cmtzvitela@gmail.com>
2022-11-16 16:14:54 -0600

    Changes made to 4.6 of set#0

--- Set0/Chapter04/Question06.js
+++ Set0/Chapter04/Question06.js
@@ -20,36 +20,28 @@ console.log("The distance between those points is ", distance([1, 2, 1], [2, 2, 
 console.log(distance(1, 2));
 
 function distance3Points(x1, y1, z1, x2, y2, z2) {
-  let dist = 0;
-  dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2) * 1.0);
-  return dist;
+  return (dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2) * 1.0));
 }
 
 function distance2Points(x1, y1, x2, y2) {
-  let dist = 0;
-  dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) * 1.0);
-  return dist;
+  return (dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) * 1.0));
 }
 
 function pointArray(coordinates) {
   if ((coordinates[0].length === 2) & (coordinates[1].length === 2)) {
     function distance2PointsArray() {
-      let dist = 0;
-      dist = Math.sqrt(
+      return (dist = Math.sqrt(
         Math.pow(coordinates[1][0] - coordinates[0][0], 2) + Math.pow(coordinates[1][1] - coordinates[0][1], 2) * 1.0
-      );
-      return dist;
+      ));
     }
     return distance2PointsArray();
   } else if ((coordinates[0].length === 3) & (coordinates[1].length === 3)) {
     function distance3PointsArray() {
-      let dist = 0;
-      dist = Math.sqrt(
+      return (dist = Math.sqrt(
         Math.pow(coordinates[1][0] - coordinates[0][0], 2) +
           Math.pow(coordinates[1][1] - coordinates[0][1], 2) +
           Math.pow(coordinates[1][2] - coordinates[0][2], 2) * 1.0
-      );
-      return dist;
+      ));
     }
     return distance3PointsArray();
   } else {
