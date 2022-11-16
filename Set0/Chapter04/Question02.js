10a372a9188377b1e43b1550ced1ad35fdbec035
unknown <cmtzvitela@gmail.com>
2022-11-16 16:37:34 -0600

    Changes to 4.2 of Set#0

--- Set0/Chapter04/Question02.js
+++ Set0/Chapter04/Question02.js
@@ -1,8 +1,8 @@
-function addRec(arr) {
-  if (arr.length === 1) {
-    return arr[0];
+function addRec(array, acc = 0, index = 0) {
+  if (index === array.length) {
+    return acc;
   } else {
-    return arr[arr.length - 1] + addRec(arr.splice(0, arr.length - 1));
+    return addRec(array, (acc += array[index]), ++index);
   }
 }
 
@@ -10,3 +10,11 @@ let numberArray = [1, 4, 6, 7, 48];
 let sum = addRec(numberArray);
 console.log("🚀 ~ file: Question02.js ~ line 11 ~ numberArray", numberArray);
 console.log("🚀 ~ file: Question02.js ~ line 11 ~ sum", sum);
+
+// function addRec(arr) {
+//   if (arr.length === 1) {
+//     return arr[0];
+//   } else {
+//     return arr[arr.length - 1] + addRec(arr.splice(0, arr.length - 1));
+//   }
+// }
