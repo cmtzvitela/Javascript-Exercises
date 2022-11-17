2650ca667bb54a59c4c13ebef70b71109d517efd
unknown <cmtzvitela@gmail.com>
2022-11-17 13:22:06 -0600

    Made client data private in 5.1 of Set#0

--- Set0/Chapter05/Question01.js
+++ Set0/Chapter05/Question01.js
@@ -4,52 +4,56 @@ class Client {
   }
 }
 
-const bank = {
-  createClient(name, accountID, firstDeposit, ownMoney) {
-    const newClient = new Client(name, accountID, firstDeposit, ownMoney);
-    bank[`${accountID}`] = { name: newClient.name, balance: newClient.firstDeposit, ownMoney: newClient.ownMoney };
-  },
-  viewBalance(accountID) {
-    if (bank[`${accountID}`]) {
-      return bank[`${accountID}`].balance;
-    }
-  },
-  withdraw(accountID, amount) {
-    if (bank[`${accountID}`]) {
-      if (bank[`${accountID}`].balance >= amount) {
-        bank[`${accountID}`].balance -= amount;
-        bank[`${accountID}`].ownMoney += amount;
+const bank = (function () {
+  const clients = {};
+  return {
+    createClient(name, accountID, firstDeposit, ownMoney) {
+      const newClient = new Client(name, accountID, firstDeposit, ownMoney);
+      clients[`${accountID}`] = { name: newClient.name, balance: newClient.firstDeposit, ownMoney: newClient.ownMoney };
+    },
+    viewBalance(accountID) {
+      if (clients[`${accountID}`]) {
+        return clients[`${accountID}`].balance;
+      }
+    },
+    withdraw(accountID, amount) {
+      if (clients[`${accountID}`]) {
+        if (clients[`${accountID}`].balance >= amount) {
+          clients[`${accountID}`].balance -= amount;
+          clients[`${accountID}`].ownMoney += amount;
+        } else {
+          throw new Error("Insufficient funds");
+        }
+      }
+      throw new Error("That's not a valid ID");
+    },
+    depositToOwnAccount(accountID, amount) {
+      if (clients[`${accountID}`].ownMoney >= amount) {
+        clients[`${accountID}`].balance += amount;
+        clients[`${accountID}`].ownMoney -= amount;
+        return clients[`${accountID}`].balance;
       } else {
         throw new Error("Insufficient funds");
       }
-    }
-    throw new Error("That's not a valid ID");
-  },
-  depositToOwnAccount(accountID, amount) {
-    if (bank[`${accountID}`].ownMoney >= amount) {
-      bank[`${accountID}`].balance += amount;
-      bank[`${accountID}`].ownMoney -= amount;
-    } else {
-      throw new Error("Insufficient funds");
-    }
-  },
-  depositToAnotherAccountFromAccount(senderID, receiverID, amount) {
-    if (bank[`${senderID}`] && bank[`${receiverID}`] && amount <= bank[`${senderID}`].balance) {
-      bank[`${receiverID}`].balance += amount;
-      bank[`${senderID}`].balance -= amount;
-    } else if (bank[`${senderID}`] && bank[`${receiverID}`] && amount >= bank[`${senderID}`].balance) {
-      throw new Error("Insufficient funds");
-    } else throw new Error("That's not a valid ID");
-  },
-  depositToAnotherAccountFromOwnMoney(senderID, receiverID, amount) {
-    if (bank[`${senderID}`] && bank[`${receiverID}`] && amount <= bank[`${senderID}`].ownMoney) {
-      bank[`${receiverID}`].balance += amount;
-      bank[`${senderID}`].ownMoney -= amount;
-    } else if (bank[`${senderID}`] && bank[`${receiverID}`] && amount >= bank[`${senderID}`].ownMoney) {
-      throw new Error("Insufficient funds");
-    } else throw new Error("That's not a valid ID");
-  },
-};
+    },
+    depositToAnotherAccountFromAccount(senderID, receiverID, amount) {
+      if (clients[`${senderID}`] && clients[`${receiverID}`] && amount <= clients[`${senderID}`].balance) {
+        clients[`${receiverID}`].balance += amount;
+        clients[`${senderID}`].balance -= amount;
+      } else if (clients[`${senderID}`] && clients[`${receiverID}`] && amount >= clients[`${senderID}`].balance) {
+        throw new Error("Insufficient funds");
+      } else throw new Error("That's not a valid ID");
+    },
+    depositToAnotherAccountFromOwnMoney(senderID, receiverID, amount) {
+      if (clients[`${senderID}`] && clients[`${receiverID}`] && amount <= clients[`${senderID}`].ownMoney) {
+        clients[`${receiverID}`].balance += amount;
+        clients[`${senderID}`].ownMoney -= amount;
+      } else if (clients[`${senderID}`] && clients[`${receiverID}`] && amount >= clients[`${senderID}`].ownMoney) {
+        throw new Error("Insufficient funds");
+      } else throw new Error("That's not a valid ID");
+    },
+  };
+})();
 
 bank.createClient("Sarah", "12345", 6000, 400);
 bank.createClient("James", "23456", 1200, 8000);
@@ -59,4 +63,3 @@ bank.depositToAnotherAccountFromAccount("12345", "23456", 1500);
 bank.depositToAnotherAccountFromOwnMoney("23456", "12345", 1000);
 console.log(bank.viewBalance("34567"));
 console.log(bank);
-bank.withdraw("34567", 2000);
