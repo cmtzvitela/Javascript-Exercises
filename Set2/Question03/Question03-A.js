79dc8ee4ecea75ca1d3b800e6fbb3dc4e8612269
unknown <cmtzvitela@gmail.com>
2022-11-17 12:58:15 -0600

    Changed 13.A in Set#3

--- Set2/Question03/Question03-A.js
+++ Set2/Question03/Question03-A.js
@@ -1,40 +1,19 @@
 const str = "7890xyz*=XYaZ`[";
 
 function moveByOne(str) {
-  var result = [];
+  return str.replaceAll(/[a-zA-Z0-9]/g, replacer);
+}
 
-  str.split("").forEach(function (i) {
-    if (/[A-Z]/.test(i)) {
-      i = i.replace(/[A-Z]/, String.fromCharCode(65 + ((i.charCodeAt() - 65 + 1) % 26)));
-    }
-    if (/[a-z]/.test(i)) {
-      i = i.replace(/[a-z]/, String.fromCharCode(97 + ((i.charCodeAt() - 97 + 1) % 26)));
-    }
-    if (/[0-9]/.test(i)) {
-      i = i.replace(/[0-9]/, String.fromCharCode(48 + ((i.charCodeAt() - 48 + 1) % 10)));
-    }
-    result.push(i);
-  });
-  return result.join("");
+function replacer(i) {
+  if (/[A-Z]/.test(i)) {
+    return (i = String.fromCharCode(65 + ((i.charCodeAt() - 65 + 1) % 26)));
+  }
+  if (/[a-z]/.test(i)) {
+    return (i = String.fromCharCode(97 + ((i.charCodeAt() - 97 + 1) % 26)));
+  }
+  if (/[0-9]/.test(i)) {
+    return (i = String.fromCharCode(48 + ((i.charCodeAt() - 48 + 1) % 10)));
+  }
 }
 
 console.log(moveByOne(str));
-
-// function moveByOne(str) {
-//   var result = [];
-
-//   str.split("").forEach(function (i) {
-//     if (/[A-Z]/.test(i)) {
-//       i = String.fromCharCode(65 + ((i.charCodeAt(0) - 65 + 1) % 26));
-//     }
-//     if (/[a-z]/.test(i)) {
-//       i = String.fromCharCode(97 + ((i.charCodeAt(0) - 97 + 1) % 26));
-//     }
-//     if (/[0-9]/.test(i)) {
-//       i = String.fromCharCode(48 + ((i.charCodeAt(0) - 48 + 1) % 10));
-//     }
-//     result.push(i);
-//   });
-
-//   return result.join("");
-// }
