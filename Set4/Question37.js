7aefbfeb43e4939d3aa60ed26960c5f664864e7a
unknown <cmtzvitela@gmail.com>
2022-12-01 17:24:28 -0600

    Fixed question 37 of Set#4

--- Set4/Question37.js
+++ Set4/Question37.js
@@ -1,14 +1,15 @@
 String.prototype.exclamation = function () {
   let newString = this;
-  return `${newString + "!"}`;
+  return `${newString}!`;
 };
 
 console.log("hello".exclamation());
 console.log("hello world".exclamation());
 
 function addNumbers(...num) {
-  const sum = num.reduce((previousValue, currentValue) => previousValue + currentValue);
+  const sum = num.reduce((previousValue, currentValue) => previousValue + currentValue, (initialValue = 0));
   return sum;
 }
 
 console.log(addNumbers(3, 4, 5, 6));
+console.log(addNumbers());
