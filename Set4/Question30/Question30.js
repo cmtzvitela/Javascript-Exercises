c6a39739d1f949c6aeb1eb0f27d967394604e315
unknown <cmtzvitela@gmail.com>
2022-12-01 17:04:14 -0600

    Made use of event delegation in questions 30 and 31 from Set#4

--- Set4/Question30/Question30.js
+++ Set4/Question30/Question30.js
@@ -3,13 +3,14 @@ const div = Array.from(document.getElementsByTagName("p"));
 for (let i = 0; i < div.length; i++) {
   if (i % 2 != 0) {
     div[i].style.visibility = "hidden";
-  } else {
-    div[i].addEventListener("click", (e) => {
-      if (div[i + 1].style.visibility == "hidden") {
-        div[i + 1].style.visibility = "visible";
-      } else {
-        div[i + 1].style.visibility = "hidden";
-      }
-    });
   }
 }
+
+document.body.addEventListener("click", (e) => {
+  const sibling = e.target.nextSibling.nextSibling;
+  if (sibling.style.visibility == "hidden") {
+    sibling.style.visibility = "visible";
+  } else {
+    sibling.style.visibility = "hidden";
+  }
+});
--- Set4/Question31/Question31.js
+++ Set4/Question31/Question31.js
@@ -3,17 +3,18 @@ const div = Array.from(document.getElementsByTagName("p"));
 for (let i = 0; i < div.length; i++) {
   if (i % 2 != 0) {
     div[i].style.visibility = "hidden";
-  } else {
-    div[i].addEventListener("click", (e) => {
-      if (div[i + 1].style.visibility == "hidden") {
-        hideAll();
-        div[i + 1].style.visibility = "visible";
-      } else {
-        div[i + 1].style.visibility = "hidden";
-      }
-    });
   }
 }
+
+document.body.addEventListener("click", (e) => {
+  const sibling = e.target.nextSibling.nextSibling;
+  if (sibling.style.visibility == "hidden") {
+    hideAll();
+    sibling.style.visibility = "visible";
+  } else {
+    sibling.style.visibility = "hidden";
+  }
+});
 
 function hideAll() {
   for (let i = 0; i < div.length; i++) {
