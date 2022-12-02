2bd89951eb86f7135b698b15dc148894f9450e12
unknown <cmtzvitela@gmail.com>
2022-12-02 13:47:29 -0600

    Changed Question 6 of Set#4

--- Set4/Question06.js
+++ Set4/Question06.js
@@ -1,6 +1,16 @@
-const str = "January February March January February March";
-function addApril(string) {
-  return str.replace(/March/g, "March April");
+const str = "January March April June";
+const months = "January February March April May June July August September October November December";
+const monthArray = months.split(" ");
+
+function completeMonths(string) {
+  const stringArray = string.split(" ");
+  console.log(stringArray[0] == monthArray[0]);
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i] !== monthArray[i]) {
+      stringArray.splice([i], 0, monthArray[i]);
+    }
+  }
+  return stringArray.toString();
 }
 
-console.log(addApril(str));
+console.log(completeMonths(str));
