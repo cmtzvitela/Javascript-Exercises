305552f9a62565af6048cd984ce93c965294d9f0
unknown <cmtzvitela@gmail.com>
2022-12-05 12:43:42 -0600

    Fixed Question 39 of Set#4

--- Set4/Question39.js
+++ Set4/Question39.js
@@ -1,56 +1,36 @@
-const qwertyOrder = "qwertyuiopasdfghjklzxcvbnm";
-const qwertyOrderBackwards = "mnbvcxzlkjhgfdsapoiuytrewq";
-function validate(string){
-  if(qwertyOrder.includes(string) || qwertyOrderBackwards.includes(string)){
-    return new Error("That can't be used")
-  }
-}
+const qwertyOrder = "qwertyuiopasdfghjklzxcvbnm1234567890";
+const qwertyOrderBackwards = "0987654321mnbvcxzlkjhgfdsapoiuytrewq";
 
-console.log(validate(""))
+const qwertyArray = qwertyOrder.split("");
+const qwertyBackwardsArray = qwertyOrderBackwards.split("");
 
 function validateString(string) {
-  const regex1 = /1234/g;
-  const regex2 = /qwert/g;
-  const regex3 = /asdf/g;
-  const regex4 = /(^|(.)(?!\2))(\d)\3{3}(?!\3)/;
-  console.log(regex3.test(string));
-
-  if (
-    regex1.test(string) ||
-    regex2.test(string) ||
-    regex3.test(string) ||
-    regex4.test(string) ||
-    validateConsecutiveSeq(string)
-  ) {
-    return new Error("That can't be used");
-  }
-  return true;
-}
-
-console.log(validateString("4123458"));
-console.log(validateString("qwert"));
-console.log(validateString("asdfghj"));
-console.log(validateString("12444458"));
-console.log(validateString("12896345"));
-
-function validateConsecutiveSeq(string) {
-  const letterArray = string.split("");
-  let isConSeq = false;
-  let previousAsciiCode = 0;
-  let numSeqcount = 0;
-
-  for (let i = 0; i < letterArray.length; i++) {
-    let asciiCode = letterArray[i].charCodeAt();
-    if (previousAsciiCode + 1 === asciiCode) {
-      numSeqcount++;
-      if (numSeqcount >= 2) {
-        isConSeq = true;
-        return false;
+  let counter = 0;
+  const stringArray = string.split("");
+  for (let i = 0; i < string.length; i++) {
+    const index = qwertyArray.indexOf(stringArray[i]);
+    if (stringArray[i + 1] === qwertyArray[index + 1]) {
+      counter++;
+      if (counter === 3) {
+        return "That sequence cannot be used";
       }
-    } else {
-      numSeqcount = 0;
     }
-    previousAsciiCode = asciiCode;
+  }
+  for (let i = 0; i < string.length; i++) {
+    const index = qwertyBackwardsArray.indexOf(stringArray[i]);
+    if (stringArray[i + 1] === qwertyBackwardsArray[index + 1]) {
+      counter++;
+      if (counter === 3) {
+        return "That sequence cannot be used";
+      }
+    }
   }
   return true;
 }
+
+console.log(validateString("wert"));
+console.log(validateString("1234"));
+console.log(validateString("asdf"));
+console.log(validateString("trew"));
+console.log(validateString("hello"));
+console.log(validateString("9485630"));
