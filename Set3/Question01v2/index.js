6c9895de81369bb8420134d28fc3a6518c408a4b
unknown <cmtzvitela@gmail.com>
2023-02-03 17:35:57 -0600

    The tab key now works on newly created notes

--- Set3/Question01v2/index.js
+++ Set3/Question01v2/index.js
@@ -1,6 +1,6 @@
 const noteFunctions = {
   getAllNotes() {
-    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+    const notes = JSON.parse(localStorage.getItem('notes') || '[]');
 
     return notes.sort((a, b) => {
       return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
@@ -9,24 +9,24 @@ const noteFunctions = {
 
   displayNotes() {
     const notes = this.getAllNotes();
-    const noteTemplate = document.getElementById("new-note");
-    const noteSection = document.getElementById("note-space");
+    const noteTemplate = document.getElementById('new-note');
+    const noteSection = document.getElementById('note-space');
     notes.forEach((note) => {
       const clonedTemplate = noteTemplate.content.cloneNode(true);
-      const noteDiv = clonedTemplate.getElementById("note-body");
+      const noteDiv = clonedTemplate.getElementById('note-body');
       noteDiv.id = `${note.id}`;
-      const noteTitle = clonedTemplate.getElementById("note-title");
+      const noteTitle = clonedTemplate.getElementById('note-title');
       noteTitle.value = `${note.title}`;
-      const noteText = clonedTemplate.getElementById("note-text");
-      noteText.addEventListener("keydown", (e) => {
+      const noteText = clonedTemplate.getElementById('note-text');
+      noteText.addEventListener('keydown', (e) => {
         if (e.keyCode === 9) {
           e.preventDefault();
-          noteText.setRangeText("\t ", noteText.selectionStart, noteText.selectionEnd, "end");
+          noteText.setRangeText('\t ', noteText.selectionStart, noteText.selectionEnd, 'end');
         }
       });
       noteText.value = `${note.body}`;
       const noteToUpdate = clonedTemplate.getElementById(`${note.id}`);
-      const span = noteToUpdate.getElementsByTagName("span");
+      const span = noteToUpdate.getElementsByTagName('span');
       const updatedDate = note.updated ? new Date(note.updated) : new Date(note.creationDate);
       span[0].textContent = `Last updated ${updatedDate.toLocaleString()}`;
       noteSection.appendChild(clonedTemplate);
@@ -37,8 +37,8 @@ const noteFunctions = {
     const notes = this.getAllNotes();
     const newNotes = notes.filter((note) => note.id != noteID);
 
-    localStorage.setItem("notes", JSON.stringify(newNotes));
-    const noteSection = document.getElementById("note-space");
+    localStorage.setItem('notes', JSON.stringify(newNotes));
+    const noteSection = document.getElementById('note-space');
     while (noteSection.firstChild) {
       noteSection.removeChild(noteSection.firstChild);
     }
@@ -47,7 +47,7 @@ const noteFunctions = {
   },
   updatedDate(existingNote) {
     const noteToUpdate = document.getElementById(existingNote.id);
-    const span = noteToUpdate.getElementsByTagName("span");
+    const span = noteToUpdate.getElementsByTagName('span');
     const date = existingNote.updated;
     span[0].textContent = `Last updated ${date.toLocaleString()}`;
   },
@@ -62,20 +62,20 @@ const noteFunctions = {
       this.updatedDate(existing);
     }
 
-    localStorage.setItem("notes", JSON.stringify(notes));
+    localStorage.setItem('notes', JSON.stringify(notes));
   },
 
   blurNote() {
-    const inpTitle = document.getElementsByClassName("note-title");
-    const inpBody = document.getElementsByClassName("note-text");
+    const inpTitle = document.getElementsByClassName('note-title');
+    const inpBody = document.getElementsByClassName('note-text');
     for (let i = 0; i < inpTitle.length; i++) {
-      inpTitle[i].addEventListener("input", (event) => {
+      inpTitle[i].addEventListener('input', (event) => {
         const updatedTitle = inpTitle[i].value;
         const updatedBody = inpBody[i].value;
         const noteID = event.currentTarget.parentNode.id;
         this.editNote({ id: noteID, title: updatedTitle, body: updatedBody });
       });
-      inpBody[i].addEventListener("input", (event) => {
+      inpBody[i].addEventListener('input', (event) => {
         const updatedTitle = inpTitle[i].value;
         const updatedBody = inpBody[i].value;
         const noteID = event.currentTarget.parentNode.id;
@@ -87,30 +87,37 @@ const noteFunctions = {
   createNote() {
     const newNote = {
       id: Math.floor(Math.random() * 1000000),
-      title: "New Note",
-      body: "Take note...",
+      title: 'New Note',
+      body: 'Take note...',
       creationDate: new Date(),
     };
-    if (!localStorage.getItem("notes")) {
-      localStorage.setItem("notes", JSON.stringify([]));
+    if (!localStorage.getItem('notes')) {
+      localStorage.setItem('notes', JSON.stringify([]));
     }
-    const notes = JSON.parse(localStorage.getItem("notes"));
+    const notes = JSON.parse(localStorage.getItem('notes'));
     notes.push(newNote);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    const noteTemplate = document.getElementById("new-note");
+    localStorage.setItem('notes', JSON.stringify(notes));
+    const noteTemplate = document.getElementById('new-note');
     const clonedTemplate = noteTemplate.content.cloneNode(true);
-    const noteSection = document.getElementById("note-space");
+    const noteSection = document.getElementById('note-space');
+    const noteText = clonedTemplate.getElementById('note-text');
+    noteText.addEventListener('keydown', (e) => {
+      if (e.keyCode === 9) {
+        e.preventDefault();
+        noteText.setRangeText('\t ', noteText.selectionStart, noteText.selectionEnd, 'end');
+      }
+    });
     noteSection.appendChild(clonedTemplate);
     this.addTrash();
     this.blurNote();
   },
 
   addTrash() {
-    const trashButton = document.getElementsByClassName("note-delete");
+    const trashButton = document.getElementsByClassName('note-delete');
 
     for (let i = 0; i < trashButton.length; i++) {
-      trashButton[i].addEventListener("click", (event) => {
-        const confirmation = confirm("The note will be deleted");
+      trashButton[i].addEventListener('click', (event) => {
+        const confirmation = confirm('The note will be deleted');
         if (confirmation) {
           noteFunctions.deleteNote(event.currentTarget.parentNode.id);
         }
@@ -120,8 +127,8 @@ const noteFunctions = {
 };
 
 noteFunctions.displayNotes();
-const createNoteButton = document.getElementById("create-note-button");
-createNoteButton.addEventListener("click", () => {
+const createNoteButton = document.getElementById('create-note-button');
+createNoteButton.addEventListener('click', () => {
   noteFunctions.createNote();
 });
 
