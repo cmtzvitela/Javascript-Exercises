9061492a19983db0930f3e18deb6823f8aeff58d
unknown <cmtzvitela@gmail.com>
2023-02-03 17:28:42 -0600

    Received the suggested code, put it there along with previous answer to compare on Set 2, 3A

--- Set2/Question03/Question03-A.js
+++ Set2/Question03/Question03-A.js
@@ -1,19 +1,34 @@
-const str = "7890xyz*=XYaZ`[";
+const str = '7890xyz*=XYaZ`[';
 
-function moveByOne(str) {
+function moveByOneFirstEdit(str) {
   return str.replaceAll(/[a-zA-Z0-9]/g, replacer);
 }
 
 function replacer(i) {
   if (/[A-Z]/.test(i)) {
-    return (i = String.fromCharCode(65 + ((i.charCodeAt() - 65 + 1) % 26)));
+    return String.fromCharCode(65 + ((i.charCodeAt() - 65 + 1) % 26));
   }
   if (/[a-z]/.test(i)) {
-    return (i = String.fromCharCode(97 + ((i.charCodeAt() - 97 + 1) % 26)));
+    return String.fromCharCode(97 + ((i.charCodeAt() - 97 + 1) % 26));
   }
   if (/[0-9]/.test(i)) {
-    return (i = String.fromCharCode(48 + ((i.charCodeAt() - 48 + 1) % 10)));
+    return String.fromCharCode(48 + ((i.charCodeAt() - 48 + 1) % 10));
   }
 }
 
-console.log(moveByOne(str));
+function cycleReplace(startChar, endChar) {
+  const offset = startChar.charCodeAt();
+  const range = endChar.charCodeAt() - offset + 1;
+  return function (c) {
+    console.log(c, offset, range)
+    return String.fromCharCode(offset + ((c.charCodeAt() - offset + 1) % range));
+  }
+}
+function moveByOne(str) {
+  return str
+    .replaceAll(/[A-Z]/g, cycleReplace('A', 'Z'))
+    .replaceAll(/[a-z]/g, cycleReplace('a', 'z'))
+    .replaceAll(/[0-9]/g, cycleReplace('0', '9'));
+}
+
+console.log(moveByOne(str))
